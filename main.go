3fbe4b6b1ba9b993c426e6567dccff68dfc0f8bc
kaneta <kaneta1011@gmail.com>
2018-07-24 00:31:50 +0900

    float64を自作のVector2に変更

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ import (
 	"image/png"
 	"io/ioutil"
 	"log"
-	"math"
 	"os"
 	"strings"
 
 	"github.com/JoshVarga/svgparser"
 	"github.com/JoshVarga/svgparser/utils"
+	"github.com/kaneta1992/vector/vector2"
 	"github.com/llgcode/draw2d/draw2dimg"
 	"github.com/pradeep-pyro/triangle"
 )
 
 var v float64 = 2048.0
 
-var pts = [][2]float64{}
+var pts = []*vec.Vector2{}
 var segs = [][2]int32{}
-var holes = [][2]float64{}
+var holes = []*vec.Vector2{}
 
 var gc *draw2dimg.GraphicContext
 
-func drawFaces(verts [][2]float64, faces [][3]int32) {
+func drawFaces(verts []*vec.Vector2, faces [][3]int32) {
 	gc.SetStrokeColor(color.RGBA{50, 50, 50, 255})
 	gc.SetLineWidth(2.0)
 	for _, f := range faces {
@@ -33,40 +33,35 @@ func drawFaces(verts [][2]float64, faces [][3]int32) {
 		p1 := verts[f[1]]
 		p2 := verts[f[2]]
 
-		x1 := p0[0]*v + v
-		y1 := p0[1]*v + v
-		x2 := p1[0]*v + v
-		y2 := p1[1]*v + v
-		x3 := p2[0]*v + v
-		y3 := p2[1]*v + v
+		p0 = p0.MulScalar(v).AddScalar(v)
+		p1 = p1.MulScalar(v).AddScalar(v)
+		p2 = p2.MulScalar(v).AddScalar(v)
 
-		gc.MoveTo(x1, y1)
-		gc.LineTo(x2, y2)
-		gc.LineTo(x3, y3)
-		gc.LineTo(x1, y1)
+		gc.MoveTo(p0.X, p0.Y)
+		gc.LineTo(p1.X, p1.Y)
+		gc.LineTo(p2.X, p2.Y)
+		gc.LineTo(p0.X, p0.Y)
 		gc.Stroke()
 	}
 }
 
-func drawSegs(verts [][2]float64, segs [][2]int32) {
+func drawSegs(verts []*vec.Vector2, segs [][2]int32) {
 	gc.SetStrokeColor(color.RGBA{50, 50, 255, 255})
 	gc.SetLineWidth(2.0)
 	for _, s := range segs {
 		p0 := verts[s[0]]
 		p1 := verts[s[1]]
 
-		x1 := p0[0]*v + v
-		y1 := p0[1]*v + v
-		x2 := p1[0]*v + v
-		y2 := p1[1]*v + v
+		p0 = p0.MulScalar(v).AddScalar(v)
+		p1 = p1.MulScalar(v).AddScalar(v)
 
-		gc.MoveTo(x1, y1)
-		gc.LineTo(x2, y2)
+		gc.MoveTo(p0.X, p0.Y)
+		gc.LineTo(p1.X, p1.Y)
 		gc.Stroke()
 	}
 }
 
-func drawPts(verts [][2]float64) {
+func drawPts(verts []*vec.Vector2) {
 	area := signedArea(verts)
 	gc.SetStrokeColor(color.RGBA{255, 50, 50, 255})
 	if area < 0.0 {
@@ -74,13 +69,12 @@ func drawPts(verts [][2]float64) {
 	}
 	gc.SetLineWidth(3.0)
 	for _, p := range verts {
-		x1 := p[0]*v + v
-		y1 := p[1]*v + v
+		p0 := p.MulScalar(v).AddScalar(v)
 
-		gc.MoveTo(x1-10, y1)
-		gc.LineTo(x1+10, y1)
-		gc.MoveTo(x1, y1-10)
-		gc.LineTo(x1, y1+10)
+		gc.MoveTo(p0.X-10, p0.Y)
+		gc.LineTo(p0.X+10, p0.Y)
+		gc.MoveTo(p0.X, p0.Y-10)
+		gc.LineTo(p0.X, p0.Y+10)
 
 		gc.Stroke()
 	}
@@ -90,44 +84,23 @@ func wrap(x, y int) int {
 	return ((x % y) + y) % y
 }
 
-func signedArea(points [][2]float64) float64 {
+func signedArea(points []*vec.Vector2) float64 {
 	lp := len(points)
 	area := 0.0
 	for i, _ := range points {
 		v1 := points[i]
 		v2 := points[wrap(i+1, lp)]
-		area += v1[0]*v2[1] - v1[1]*v2[0]
+		area += v1.Cross(v2)
 	}
 	return area
 }
 
-func subVec2(v1, v2 [2]float64) [2]float64 {
-	return [2]float64{v1[0] - v2[0], v1[1] - v2[1]}
-}
-
-func addVec2(v1, v2 [2]float64) [2]float64 {
-	return [2]float64{v1[0] + v2[0], v1[1] + v2[1]}
-}
-
-func mulVec2(v [2]float64, s float64) [2]float64 {
-	return [2]float64{v[0] * s, v[1] * s}
-}
-
-func length(v [2]float64) float64 {
-	return math.Sqrt(v[0]*v[0] + v[1]*v[1])
-}
-
-func normalize(v [2]float64) [2]float64 {
-	l := length(v)
-	return [2]float64{v[0] / l, v[1] / l}
-}
-
-func getPath(points [][2]float64) ([][2]float64, [][2]int32, [][2]float64) {
+func getPath(points []*vec.Vector2) ([]*vec.Vector2, [][2]int32, []*vec.Vector2) {
 	nextIndex := len(pts)
 	lp := len(points)
-	retPts := [][2]float64{}
+	retPts := []*vec.Vector2{}
 	retSegs := [][2]int32{}
-	retHoles := [][2]float64{}
+	retHoles := []*vec.Vector2{}
 
 	for i, p := range points {
 		retPts = append(retPts, p)
@@ -142,18 +115,18 @@ func getPath(points [][2]float64) ([][2]float64, [][2]int32, [][2]float64) {
 		v1 := points[1]
 		v2 := points[2]
 		// 各頂点へのベクトル
-		e0 := subVec2(v0, v1)
-		e1 := subVec2(v2, v1)
+		e0 := v0.Sub(v1)
+		e1 := v2.Sub(v1)
 		// ハーフベクトル
-		hv := normalize(addVec2(e0, e1))
+		hv := e0.Add(e1).Normalize()
 		// 起点頂点のハーフベクトルに少し動かした場所を穴とする
-		triArea := signedArea([][2]float64{v0, v1, v2})
+		triArea := signedArea([]*vec.Vector2{v0, v1, v2})
 		if triArea < 0.0 {
 			// 起点の三角形が左回りなら内向きなのでハーフベクトル方向へ
-			retHoles = append(retHoles, addVec2(v1, mulVec2(hv, 0.00001)))
+			retHoles = append(retHoles, v1.Add(hv.MulScalar(0.00001)))
 		} else {
 			// 右回りなら外向きなのでハーフベクトルの逆方向へ
-			retHoles = append(retHoles, subVec2(v1, mulVec2(hv, 0.00001)))
+			retHoles = append(retHoles, v1.Sub(hv.MulScalar(0.00001)))
 		}
 	}
 	return retPts, retSegs, retHoles
@@ -163,7 +136,7 @@ func putsRect(x, y, w, h float64) {
 	hw := w / 2.0
 	hh := h / 2.0
 	// 右回りで配置する
-	p, s, ho := getPath([][2]float64{
+	p, s, ho := getPath([]*vec.Vector2{
 		{x - hw, y - hh}, {x + hw, y - hh}, {x + hw, y + hh}, {x - hw, y + hh},
 	})
 	pts = append(pts, p...)
@@ -175,7 +148,7 @@ func cutsRect(x, y, w, h float64) {
 	hw := w / 2.0
 	hh := h / 2.0
 	// 左回りで配置する
-	p, s, ho := getPath([][2]float64{
+	p, s, ho := getPath([]*vec.Vector2{
 		{x - hw, y - hh}, {x - hw, y + hh}, {x + hw, y + hh}, {x + hw, y - hh},
 	})
 	pts = append(pts, p...)
@@ -183,7 +156,7 @@ func cutsRect(x, y, w, h float64) {
 	holes = append(holes, ho...)
 }
 
-func putPath(points [][2]float64) {
+func putPath(points []*vec.Vector2) {
 	p, s, h := getPath(points)
 	pts = append(pts, p...)
 	segs = append(segs, s...)
@@ -191,11 +164,11 @@ func putPath(points [][2]float64) {
 }
 
 func main() {
-	holes = [][2]float64{
+	holes = []*vec.Vector2{
 		{99999.9, 99999.9}, // 穴がない時用の点
 	}
 
-	svg, err := ioutil.ReadFile("q.svg")
+	svg, err := ioutil.ReadFile("nazo.svg")
 	if err != nil {
 		log.Fatal("io error")
 	}
@@ -210,30 +183,34 @@ func main() {
 	gc = draw2dimg.NewGraphicContext(rgba)
 
 	// TODO: 頂点をみて自動で正規化する
-	rate := 5000.0
+	irate := 1.0 / 5000.0
 	for i, subpath := range path.Subpaths {
 		fmt.Printf("Path %d: ", i)
-		points := [][2]float64{}
-		var nowPoint []float64
+		points := []*vec.Vector2{}
+		var nowPoint *vec.Vector2
 		for _, command := range subpath.Commands {
 			param := []float64(command.Params)
 			switch command.Symbol {
 			case "M":
-				points = append(points, [][2]float64{{param[0] / rate, param[1] / rate}}...)
-				nowPoint = []float64{param[0], param[1]}
+				v1 := &vec.Vector2{param[0], param[1]}
+				points = append(points, v1.MulScalar(irate))
+				nowPoint = v1
 			case "L":
-				points = append(points, [][2]float64{{param[0] / rate, param[1] / rate}}...)
-				nowPoint = []float64{param[0], param[1]}
+				v1 := &vec.Vector2{param[0], param[1]}
+				points = append(points, v1.MulScalar(irate))
+				nowPoint = v1
 			case "Q":
-				area := signedArea([][2]float64{{nowPoint[0], nowPoint[1]}, {param[0], param[1]}, {param[2], param[3]}})
+				v1 := &vec.Vector2{param[0], param[1]}
+				v2 := &vec.Vector2{param[2], param[3]}
+				area := signedArea([]*vec.Vector2{nowPoint, v1, v2})
 				if area < 0.0 {
 					// 左回りなら制御点が内部(右)にいるので制御点も含めた頂点を追加する
-					points = append(points, [][2]float64{{param[0] / rate, param[1] / rate}, {param[2] / rate, param[3] / rate}}...)
+					points = append(points, []*vec.Vector2{v1.MulScalar(irate), v2.MulScalar(irate)}...)
 				} else {
 					// 右回りなら制御点が外部(左)にいるので制御点を無視する
-					points = append(points, [2]float64{param[2] / rate, param[3] / rate})
+					points = append(points, v2.MulScalar(irate))
 				}
-				nowPoint = []float64{param[2], param[3]}
+				nowPoint = v2
 			default:
 			}
 		}
@@ -251,7 +228,7 @@ func main() {
 	//putsRect(0.2, 0.3, 0.3, 0.2)
 	//putsRect(-0.2, -0.3, 0.3, 0.2)
 
-	verts, faces := triangle.ConstrainedDelaunay(pts, segs, holes)
+	verts, faces := triangle.ConstrainedDelaunay(vec.Vec2ToFloat64(pts), segs, vec.Vec2ToFloat64(holes))
 	log.Print(pts)
 	log.Print(segs)
 	log.Print(holes)
@@ -259,7 +236,7 @@ func main() {
 	log.Print(verts)
 	log.Print(faces)
 
-	drawFaces(verts, faces)
+	drawFaces(vec.Float64ToVec2(verts), faces)
 	//drawPts(holes)
 	//drawSegs(verts, segs)
 
